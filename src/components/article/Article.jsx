ae56f7ae876c86120576e7e4411996b002762a81
shainsethi <83071544+shainsethi@users.noreply.github.com>
2023-07-18 02:42:10 +0530

    Add files via upload

---
+++ src/components/article/Article.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './article.css';
+
+const article = (props) => {
+  return (
+    <div className='gpt3__blog-container_article'>
+      <div className='gpt3__blog-container_article-image'>
+        <img src={props.imgUrl} alt='blog'/>
+      </div>
+      <div className='gpt3__blog-container_article-content'>
+          <div>
+            <p>{props.date}</p>
+            <h3>{props.title}</h3>
+          </div>
+          <p>Read Full Article</p>
+      </div>
+    </div>
+  )
+}
+
+export default article
